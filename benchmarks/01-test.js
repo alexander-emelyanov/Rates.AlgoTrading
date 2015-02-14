0636518aa6bc484fbcf5962114337b098cc82fab
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2015-02-14 07:28:56 +0300

    Draft for first load test

--- benchmarks/01-test.js
+++ benchmarks/01-test.js
@@ -9,17 +9,19 @@ var raatOpts = {port: optPort, prefix: optPrefix};
 // Run server
 new Raat(raatOpts);
 
+// Run clients
+
 var clients = [], i = 0;
 var testSize = 100000;
 
 var RaatClient = function(i){
     var ws;
-    ws = new WebSocket('ws://127.0.0.1:8002/raat/websocket');
+    ws = new WebSocket('ws://127.0.0.1:' + optPort + '/' + optPrefix + '/websocket');
     ws.on('open', function() {
-        // ws.send('Current time: ' + process.hrtime());
+        ws.send('Current time: ' + process.hrtime());
     });
     ws.on('message', function(msg) {
-        // console.log('CLIENT: Received: %s', msg);
+        console.log('CLIENT: Received: %s', msg);
     });
     ws.on('close', function() {
         console.log('CLIENT: Connection closed');
---
+++ benchmarks/02-send-and-receive.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var Raat = require('./../lib/raat').Raat;
+var SockJS = require('sockjs-client-node');
+
+var optPort = 8002, optPrefix = 'raat';
+var raatOpts = {port: optPort, prefix: optPrefix};
+
+// Run server
+new Raat(raatOpts);
+
+// Raat clients
+var testSize = 3;
+
+var RaatClient = function(opts){
+
+    var that = this;
+
+    var messagesHistory = [];
+
+    this.connection = new SockJS('http://127.0.0.1:' + optPort + '/' + optPrefix);
+
+    this.connection.onopen = function() {
+        if (opts && typeof opts.onOpen == 'function'){
+            opts.onOpen(that);
+        }
+    };
+
+    this.connection.onmessage = function(message) {
+        try{
+            var messageObject = JSON.parse(message.data);
+            if (opts && typeof opts.onMessage == 'function'){
+                opts.onMessage(that, messageObject);
+            }
+        } catch (e){
+            console.log('CLIENT: Invalid message JSON format');
+        }
+    };
+
+    this.connection.onclose = function() {
+        //console.log('CLIENT: Connection closed');
+    };
+
+    this.connection.onerror = function(err) {
+        //console.log('CLIENT: Error #%s %s', i, err);
+        // throw err;
+    };
+};
+
+RaatClient.prototype.sendMessage = function(channel, message){
+    message.channel = channel;
+    this.connection.send(JSON.stringify(message));
+};
+
+RaatClient.prototype.subscribe = function(channel){
+    this.sendMessage(channel, {command: 'subscribe'});
+};
+
+var consumer = new RaatClient({
+    onOpen: function(raatClient){
+        raatClient.subscribe('EUR\\USD');
+    },
+    onMessage: function(raatClient, messageObject){
+        console.log('Consumer received a message', messageObject);
+    }
+});
+//consumer.subscribe('rates:EURUSD');
+new RaatClient({onOpen: function(raatClient){
+    for(var i = 0; i < testSize; i++){
+        raatClient.sendMessage('EUR\\USD', {rate: Math.random()});
+    }
+}});
